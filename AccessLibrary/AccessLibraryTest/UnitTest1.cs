0f4821fa2f47d2cb1db7e03333cbd10a05bdb2a5
Skyler Swenson <turtlelink2@gmail.com>
2015-11-16 23:16:37 -0800

    Did a bunch of mock db stuff, Created employeeTest

---
+++ AccessLibrary/AccessLibraryTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccessLibraryTest
+{
+	[TestClass]
+	public class EmployeeTest
+	{
+		[TestMethod]
+		public void EmployeeLoginTest()
+		{
+			PointOfSales.Users.Employee SallySue = MockConnections.MockLogin.Login("SallySue", "abc123");
+
+			Assert.AreEqual("123-123-1234", SallySue.phoneNumber);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(InvalidOperationException))]
+		public void EmployeeIncorrectLoginTest()
+		{
+			PointOfSales.Users.Employee SallySue = MockConnections.MockLogin.Login("SallySue", "WRONG_PASSWORD");
+		}
+	}
+}
