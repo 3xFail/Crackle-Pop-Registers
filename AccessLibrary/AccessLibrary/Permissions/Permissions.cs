cc69832a31807708c0fcd06f6ef4002ad69c64f4
Skyler Swenson <turtlelink2@gmail.com>
2015-11-16 20:06:06 -0800

    Started writing permissions class.

---
+++ AccessLibrary/AccessLibrary/Permissions/Permissions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointOfSales.Permissions
+{
+	class Permissions
+	{
+		public enum RegisterPermissions
+		{
+			UseRegister = 1,
+			ProcessPayment = 2,
+			PriceOverride = 4,
+			ApplyCoupon = 8,
+		};
+	}
+}
