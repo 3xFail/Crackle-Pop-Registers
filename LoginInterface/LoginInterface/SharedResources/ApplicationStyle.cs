158216e9abd6fdc57fb1af5cf67707a47022e1a8
Phillip Smith <phillip.smith@oit.edu>
2016-01-21 17:45:50 -0800

    finally fixed it

---
+++ LoginInterface/LoginInterface/SharedResources/ApplicationStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snap_Register_System_Interface.Resources
+{
+	public partial class ApplicationStyle : ResourceDictionary
+	{
+	}
+}
