7f5dc0b6f51700176cbc36629dfd91cb406e0ce0
SashaIliyn <alexandramiliyn@gmail.com>
2016-01-21 17:50:49 -0800

    Added individual tab window for each button.

--- Snap Admin System Interface/Snap Admin System Interface/MainWindow.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/MainWindow.xaml.cs
@@ -27,59 +27,57 @@ namespace Snap_Admin_System_Interface
 
         private void btn_Inv(object sender, RoutedEventArgs e)
         {
-           // TabControl tab_inv = new TabControl();
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            //MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Visible;
+            AnalysisTabs.Visibility = Visibility.Collapsed;
+            EmployeesTabs.Visibility = Visibility.Collapsed;
+            CustomersTabs.Visibility = Visibility.Collapsed;
+            SalesTabs.Visibility = Visibility.Collapsed;
+            OptionsTabs.Visibility = Visibility.Collapsed;
         }
         private void btn_Anal(object sender, RoutedEventArgs e)
         {
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Collapsed;
+            AnalysisTabs.Visibility = Visibility.Visible;
+            EmployeesTabs.Visibility = Visibility.Collapsed;
+            CustomersTabs.Visibility = Visibility.Collapsed;
+            SalesTabs.Visibility = Visibility.Collapsed;
+            OptionsTabs.Visibility = Visibility.Collapsed;
         }
         private void btn_Emp(object sender, RoutedEventArgs e)
         {
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Collapsed;
+            AnalysisTabs.Visibility = Visibility.Collapsed;
+            EmployeesTabs.Visibility = Visibility.Visible;
+            CustomersTabs.Visibility = Visibility.Collapsed;
+            SalesTabs.Visibility = Visibility.Collapsed;
+            OptionsTabs.Visibility = Visibility.Collapsed;
         }
         private void btn_Cust(object sender, RoutedEventArgs e)
         {
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Collapsed;
+            AnalysisTabs.Visibility = Visibility.Collapsed;
+            EmployeesTabs.Visibility = Visibility.Collapsed;
+            CustomersTabs.Visibility = Visibility.Visible;
+            SalesTabs.Visibility = Visibility.Collapsed;
+            OptionsTabs.Visibility = Visibility.Collapsed;
         }
         private void btn_Sales(object sender, RoutedEventArgs e)
         {
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Collapsed;
+            AnalysisTabs.Visibility = Visibility.Collapsed;
+            EmployeesTabs.Visibility = Visibility.Collapsed;
+            CustomersTabs.Visibility = Visibility.Visible;
+            SalesTabs.Visibility = Visibility.Visible;
+            OptionsTabs.Visibility = Visibility.Collapsed;
         }
         private void btn_Opt(object sender, RoutedEventArgs e)
         {
-            //int newIndex = Tabs.SelectedIndex + 1;
-            //if (newIndex >= Tabs.Items.Count)
-            //    newIndex = 0;
-            //Tabs.SelectedIndex = newIndex;
-
-            MessageBox.Show("Selected tab: " + (Tabs.SelectedItem as TabItem).Header);
+            InventoryTabs.Visibility = Visibility.Collapsed;
+            AnalysisTabs.Visibility = Visibility.Collapsed;
+            EmployeesTabs.Visibility = Visibility.Collapsed;
+            CustomersTabs.Visibility = Visibility.Collapsed;
+            SalesTabs.Visibility = Visibility.Collapsed;
+            OptionsTabs.Visibility = Visibility.Visible;
         }
-
-    }
+    }   
 }
