ce85bd00a2c1bbeb2b7f1060a3893123c534be5f
Skyler Swenson <turtlelink2@gmail.com>
2016-01-23 22:41:27 -0800

    Made Item Submenu, might need to change code to seperate interface from buisiness layer.

---
+++ Snap Register System Interface/Snap Register System Interface/EditItemMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SnapRegisters
+{
+	/// <summary>
+	/// Interaction logic for EditItemMenu.xaml
+	/// </summary>
+	public partial class EditItemMenu : Window
+	{
+		public EditItemMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void CloseWindow(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
