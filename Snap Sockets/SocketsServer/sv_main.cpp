5b4af643f04e5992fd3ae39d6bb8d3595753dc9b
Phillip Smith <phillip.smith@oit.edu>
2016-02-28 15:25:36 -0800

    server now defaults to port 6119

--- Snap Sockets/SocketsServer/sv_main.cpp
+++ Snap Sockets/SocketsServer/sv_main.cpp
@@ -36,54 +36,62 @@ using std::exception;
 
 db_conn_str load_connection_string();
 
-int main( int argc, char ** argv )
+int main(int argc, char ** argv)
 {
-	if( argc < 2 )
+	//if (argc < 2)
+	//{
+	//	cout << "Required port number not passed.\n";
+	//}
+
+	// Hope you don't mind if I set the port to default to 6119
+	int port = 0;
+
+	if (argc >= 2)
 	{
-		cout << "Required port number not passed.\n";
-		return 1;
+		port = atoi(argv[1]);
 	}
 
-	int port = atoi( argv[1] );
-	if( port == 0 )
+	if (port == 0)
 	{
-		cout << "Valid port number not passed.\n";
-		return 1;
+		port = 6119;
+		cout << "Valid port number not passed. Defaulting to 6119.\n";
+		//cout << "Valid port number not passed.\n";
+		//return 1;
 	}
 
 	try
 	{
-		sql_singleton::instance().open( load_connection_string().get() );
+		sql_singleton::instance().open(load_connection_string().get());
 		cout << "Sucessfully connected to database.\n";
 
 		io_service io_service;
-		server server( io_service, port );
+		server server(io_service, port);
 
 		cout << "Server is listening on port " << port << "...\n";
 		io_service.run();
 	}
-	catch( const cppdb_error & e ) //if it's a database error we'll catch that
+	catch (const cppdb_error & e) //if it's a database error we'll catch that
 	{
 		cout << "Database Exception: " << e.what() << '\n';
 	}
-	catch( const exception & e ) //otherwise who knows what went wrong, jesus.
+	catch (const exception & e) //otherwise who knows what went wrong, jesus.
 	{
 		cout << "General Exception: " << e.what() << "\n";
 	}
 
-	system( "pause" );
+	system("pause");
 	return 0;
 }
 
 db_conn_str load_connection_string()
 {
-	ifstream file( "database_connection_info.txt" );
-	if( file.is_open() )
+	ifstream file("database_connection_info.txt");
+	if (file.is_open())
 	{
 		string server, db, user, pass;
 		file >> server >> db >> user >> pass;
-		return db_conn_str( server, db, user, pass );
+		return db_conn_str(server, db, user, pass);
 	}
-	else throw exception( "Cannot open database_connection_info.txt" );
+	else throw exception("Cannot open database_connection_info.txt");
 }
 
