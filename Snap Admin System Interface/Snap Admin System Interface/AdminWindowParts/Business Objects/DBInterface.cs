f9c0cfdaa8a2a87300184f3e09590a857a7c8590
Phillip Smith <phillip.smith@oit.edu>
2016-03-08 20:32:31 -0800

    Added DBInterface class

--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/Business Objects/DBInterface.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/Business Objects/DBInterface.cs
@@ -1,13 +1,51 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CSharpClient;
 
 namespace Snap_Admin_System_Interface.AdminWindowParts.Business_Objects
 {
     static class DBInterface
     {
+        public static void AddEmployee(connection_session connection, string firstName, string lastName, string username,
+            string email, string password, string authorizationLevel, DateTime DOB,
+            string phoneNumber, string address_1, string address_2, string city, string state, string country,
+            string zip)
+        {
+            string dob_string = DOB == null ? string.Empty : DOB.ToString();
 
+
+
+            connection.write(string.Format("AddUser \"{0}\" \"{1}\" \"{2}\" \"{3}\" \"{4}\" \"{5}\" \"{6}\" \"{7}\" \"{8}\" \"{9}\" \"{10}\" \"{11}\" \"{12}\" \"{13}\" \"{14}\" ",
+                firstName, lastName, username, password, phoneNumber, authorizationLevel, "1", dob_string, address_1, address_2, city, state, country, zip, email));
+
+            if (connection.Response[0].Get("UserID") == "-1") //otherwise the UserID returned is the ID of the account just created
+                throw new InvalidOperationException("Username already exists. Please choose another.");    
+        }
+
+        public static void AddItem(connection_session connection, string name, string price, string barcode)
+        {
+
+            connection.write(string.Format("AddItem \"{0}\" \"{1}\" \"{2}\" \"{3}\" ", name, price, barcode, "1"));
+
+            if (connection.Response[0].Get("ItemID") == "-1")
+                throw new InvalidOperationException("Item with barcode \"" + barcode + "\" already exists.");
+        }
+
+        public static void AddCust(connection_session connection, string firstName, string lastName, string address_1, 
+            string address_2, string city, string state, string country, string zip, string phoneNumber, string email,
+            DateTime DOB)
+        {
+            string dob_string = DOB == null ? string.Empty : DOB.ToString();
+
+            connection.write(string.Format("AddCust \"{0}\" \"{1}\" \"{2}\" \"{3}\" \"{4}\" \"{5}\" \"{6}\" \"{7}\" \"{8}\" \"{9}\" \"{10}\" \"{11}\" \"{12}\" ",
+                firstName, lastName, address_1, address_2, city, state, country, zip, phoneNumber, email, "NULL", "1", dob_string));
+
+            if (connection.Response[0].Get("UserID") == "-1")
+                throw new InvalidOperationException("User with phone number \"" + phoneNumber + "\" already exists.");
+        }
     }
 }
