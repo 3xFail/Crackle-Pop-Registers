be9ac8dc85378269e09d179cc3e7f6587fd9c8e5
SashaIliyn <alexandramiliyn@gmail.com>
2016-03-08 20:55:44 -0800

    Updated Employee info form and got states working.

--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/AddEmployeePage.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/AddEmployeePage.xaml.cs
@@ -20,9 +20,65 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
     /// </summary>
     public partial class AddEmployeePage : Page
     {
+        // list of all US states
+        private static String states = "|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY|";
+
         public AddEmployeePage()
         {
             InitializeComponent();
         }
+
+        // changing date to selected date
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // get DatePicker reference
+            var picker = sender as DatePicker;
+
+            // get nullable DateTime from SelectedDate
+            DateTime? date = picker.SelectedDate;
+            if (date == null)
+            {
+                // a null object
+                this.Title = "No date";
+            }
+            else
+            {
+                // no need to display the time
+                this.Title = date.Value.ToShortDateString();
+                //textBoxMM = Day.
+            }
+        }
+        // load the list of items in combobox
+        private void State_Loaded(object sender, RoutedEventArgs e)
+        {
+            // gGet the ComboBox reference
+            var comboBox = sender as ComboBox;
+
+            // assign the ItemsSource to the List
+            comboBox.ItemsSource = states.Split('|');
+
+            // make the first item selected
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void State_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // get the ComboBox
+            var comboBox = sender as ComboBox;
+
+            // set SelectedItem as Window Title
+            string value = comboBox.SelectedItem as string;
+            this.Title = value;
+        }
+
+
+        // determines which US state to use
+        public static bool isStateAbbreviation(String state)
+        {
+            return state.Length == 2 && states.IndexOf(state) > 0;
+        }
+
+
+
     }
 }
