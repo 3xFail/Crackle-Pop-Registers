f7317f3c875a1b4aa3c16651ceb64e15c5a5ba97
SashaIliyn <alexandramiliyn@gmail.com>
2016-03-09 21:34:35 -0800

    Added Snap logo to Admin.

--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/AddEmployeePage.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/AddEmployeePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SnapRegisters;
 
 namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
 {
@@ -20,9 +21,11 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
     /// </summary>
     public partial class AddEmployeePage : Page
     {
-        // list of all US states
+        // all US states
         private static String states = "Select a State|AL|AK|AS|AZ|AR|CA|CO|CT|DE|DC|FM|FL|GA|GU|HI|ID|IL|IN|IA|KS|KY|LA|ME|MH|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|MP|OH|OK|OR|PW|PA|PR|RI|SC|SD|TN|TX|UT|VT|VI|VA|WA|WV|WI|WY|";
+        // all the countries
         private static String countries = "Select a Country|Afghanistan|Åland Islands|Albania|Algeria|American Samoa|Andorra|Angola|Anguilla|Antigua and Barbuda|Argentina|Armenia|Aruba|Australia|Austria|Azerbaijan|Bangladesh|Barbados|Bahamas|Bahrain|Belarus|Belgium|Belize|Benin|Bermuda|Bhutan|Bolivia|Bosnia and Herzegovina|Botswana|Brazil|British Indian Ocean Territory|British Virgin Islands|Brunei Darussalam|Bulgaria|Burkina Faso|Burma|Burundi|Cambodia|Cameroon|Canada|Cape Verde|Cayman Islands|Central African Republic|Chad|Chile|China|Christmas Island|Cocos (Keeling) Islands|Colombia|Comoros|Congo-Brazzaville|Congo-Kinshasa|Cook Islands|Costa Rica|$_[|Croatia|Curaçao|Cyprus|Czech Republic|Denmark|Djibouti|Dominica|Dominican Republic|East Timor|Ecuador|El Salvador|Egypt|Equatorial Guinea|Eritrea|Estonia|Ethiopia|Falkland Islands|Faroe Islands|Federated States of Micronesia|Fiji|Finland|France|French Guiana|French Polynesia|French Southern Lands|Gabon|Gambia|Georgia|Germany|Ghana|Gibraltar|Greece|Greenland|Grenada|Guadeloupe|Guam|Guatemala|Guernsey|Guinea|Guinea-Bissau|Guyana|Haiti|Heard and McDonald Islands|Honduras|Hong Kong|Hungary|Iceland|India|Indonesia|Iraq|Ireland|Isle of Man|Israel|Italy|Jamaica|Japan|Jersey|Jordan|Kazakhstan|Kenya|Kiribati|Kuwait|Kyrgyzstan|Laos|Latvia|Lebanon|Lesotho|Liberia|Libya|Liechtenstein|Lithuania|Luxembourg|Macau|Macedonia|Madagascar|Malawi|Malaysia|Maldives|Mali|Malta|Marshall Islands|Martinique|Mauritania|Mauritius|Mayotte|Mexico|Moldova|Monaco|Mongolia|Montenegro|Montserrat|Morocco|Mozambique|Namibia|Nauru|Nepal|Netherlands|New Caledonia|New Zealand|Nicaragua|Niger|Nigeria|Niue|Norfolk Island|Northern Mariana Islands|Norway|Oman|Pakistan|Palau|Panama|Papua New Guinea|Paraguay|Peru|Philippines|Pitcairn Islands|Poland|Portugal|Puerto Rico|Qatar|Réunion|Romania|Russia|Rwanda|Saint Barthélemy|Saint Helena|Saint Kitts and Nevis|Saint Lucia|Saint Martin|Saint Pierre and Miquelon|Saint Vincent|Samoa|San Marino|São Tomé and Príncipe|Saudi Arabia|Senegal|Serbia|Seychelles|Sierra Leone|Singapore|Sint Maarten|Slovakia|Slovenia|Solomon Islands|Somalia|South Africa|South Georgia|South Korea|Spain|Sri Lanka|Sudan|Suriname|Svalbard and Jan Mayen|Sweden|Swaziland|Switzerland|Syria|Taiwan|Tajikistan|Tanzania|Thailand|Togo|Tokelau|Tonga|Trinidad and Tobago|Tunisia|Turkey|Turkmenistan|Turks and Caicos Islands|Tuvalu|Uganda|Ukraine|United Arab Emirates|United Kingdom|United States|Uruguay|Uzbekistan|Vanuatu|Vatican City|Vietnam|Venezuela|Wallis and Futuna|Western Sahara|Yemen|Zambia|Zimbabwe|";
+        // the three permission levels
         private static String permissions = "Select an Authorization|Cashier|Manager|Owner|";
 
         public AddEmployeePage()
@@ -49,44 +52,44 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
                 this.Title = date.Value.ToShortDateString();
             }
         }
-        // load the list of items in combobox
+        // load the states items in combobox
         private void State_Loaded(object sender, RoutedEventArgs e)
         {
-            // gGet the ComboBox reference
+            // gGet the combobox reference
             var comboBox = sender as ComboBox;
 
-            // assign the ItemsSource to the List
+            // assign the ItemsSource to the states
             comboBox.ItemsSource = states.Split('|');
 
             // make the first item selected
             comboBox.SelectedIndex = 0;
         }
-
+        // select the state
         private void State_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // get the ComboBox
+            // get the combobox
             var comboBox = sender as ComboBox;
 
             // set SelectedItem as Window Title
             string value = comboBox.SelectedItem as string;
             this.Title = value;
         }
-        // load the list of items in combobox
+        // load the permission items in combobox
         private void Authorization_Loaded(object sender, RoutedEventArgs e)
         {
-            // gGet the ComboBox reference
+            // get the combobox reference
             var comboBox = sender as ComboBox;
 
-            // assign the ItemsSource to the List
+            // assign the ItemsSource to the permission levels
             comboBox.ItemsSource = permissions.Split('|');
 
             // make the first item selected
             comboBox.SelectedIndex = 0;
         }
-
+        // select the permission level
         private void Authorization_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // get the ComboBox
+            // get the combobox
             var comboBox = sender as ComboBox;
 
             // set SelectedItem as Window Title
@@ -96,33 +99,45 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
         // load the list of items in combobox
         private void Country_Loaded(object sender, RoutedEventArgs e)
         {
-            // gGet the ComboBox reference
+            // get the combobox reference
             var comboBox = sender as ComboBox;
 
-            // assign the ItemsSource to the List
+            // assign the ItemsSource to the countries
             comboBox.ItemsSource = countries.Split('|');
 
             // make the first item selected
             comboBox.SelectedIndex = 0;
         }
-
+        // select the country
         private void Country_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            // get the ComboBox
+            // get the combobox
             var comboBox = sender as ComboBox;
 
             // set SelectedItem as Window Title
             string value = comboBox.SelectedItem as string;
             this.Title = value;
         }
-
-        // determines which US state to use
+        // determines the US state
         public static bool isStateAbbreviation(String state)
         {
             return state.Length == 2 && states.IndexOf(state) > 0;
         }
-
-
-
+        // submits the input values into the database
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            if( textBoxFirstName.Text == string.Empty )
+                System.Windows.Forms.MessageBox.Show("First name is required. Please enter it.");
+            else if (textBoxUsername.Text == string.Empty)
+                System.Windows.Forms.MessageBox.Show("Username is required. Please enter it.");
+            else if (textBoxPassword.Password == string.Empty)
+                System.Windows.Forms.MessageBox.Show("Password is required. Please enter it.");
+            else if (textBoxPhone.Text == string.Empty)
+                System.Windows.Forms.MessageBox.Show("Phone number is required. Please enter it.");
+            else if (textBoxAuthorization.Text == string.Empty)
+                System.Windows.Forms.MessageBox.Show("Authorization level is required. Please select one.");
+            else
+                DBInterface.AddEmployee(textBoxFirstName.Text, textBoxLastName.Text, textBoxUsername.Text, textBoxEmail.Text, textBoxPassword.Password, textBoxAuthorization.Text, DOB.DisplayDate, textBoxPhone.Text, textBoxAddress1.Text, textBoxAddress2.Text, textBoxCity.Text, textBoxState.Text, textBoxCountry.Text, textBoxZip.Text);
+        }
     }
 }
