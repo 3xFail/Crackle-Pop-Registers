60f83f7eb581f82812745e2ccb54eea90d6927eb
David Jacob Asmuth <david.asmuth@oit.edu>
2016-03-16 09:53:59 -0700

    Reduced code duplication.

--- Snap Sockets/CSharpClient/ConnectionSession.cs
+++ Snap Sockets/CSharpClient/ConnectionSession.cs
@@ -9,26 +9,7 @@ namespace CSharpClient
         private readonly string ConnectionString = @"Data Source=aura.students.cset.oit.edu;Initial Catalog=snap;User ID=snap_admin;Password=snap_admin;";
         public ConnectionSession( string username, string password ) //Constructor needs similar code to 'Write' but not the same because of authed check and whatnot.
         {
-            using( SqlConnection Connection = new SqlConnection( ConnectionString ) )
-            {
-                SqlCommand Command = new SqlCommand( "GetEmployee_UsernamePassword @0, @1", Connection );
-                Command.Parameters.AddWithValue( "@0", username );
-                Command.Parameters.AddWithValue( "@1", password );
-
-                var reader = Command.ExecuteReader();
-                var doc = new XmlDocument();
-
-                if( reader.Read() )
-                {
-                    try
-                    {
-                        doc.LoadXml( @"<root>" + reader[0] + @"</root>" );
-                    }
-                    catch( XmlException ) { }
-                }
-                Response = doc.GetElementsByTagName( "row" );
-                Connection.Close();
-            }
+            QueryDB( "GetEmployee_UsernamePassword @0, @1", username, password );
 
             Authed = Response.Count == 1;
             if( !Authed )
@@ -37,32 +18,42 @@ namespace CSharpClient
 
         public void Write( string proc, params object[] args )
         {
-            if( !Authed ) throw new InvalidOperationException( "Un-Authed user cannot perform this action" );
+            if( !Authed )
+                throw new InvalidOperationException( "Un-Authed user cannot perform this action" );
 
+            QueryDB( proc, args );
+        }
+
+        private void QueryDB( string proc, params object[] args )
+        {
             using( SqlConnection Connection = new SqlConnection( ConnectionString ) )
             {
                 Connection.Open();
-
-                SqlCommand Command = new SqlCommand( proc, Connection );
-                for( int i = 0; i < args.Length; ++i )
-                    Command.Parameters.AddNullable( "@" + i.ToString(), args[i] );
-
-                var reader = Command.ExecuteReader();
-                var doc = new XmlDocument();
-
-                if( reader.Read() )
+                using( SqlCommand Command = new SqlCommand( proc, Connection ) )
                 {
-                    try
-                    {
-                        doc.LoadXml( @"<root>" + reader[0] + @"</root>" );
-                    }
-                    catch( XmlException ) { }
+                    for( int i = 0; i < args.Length; ++i )
+                        Command.Parameters.AddNullable( "@" + i.ToString(), args[i] );
+
+                    LoadResponse( Command.ExecuteReader() );
                 }
-                Response = doc.GetElementsByTagName( "row" );
                 Connection.Close();
             }
         }
-        public XmlNodeList Response { get; set; }
+
+        private void LoadResponse( SqlDataReader reader )
+        {
+            var doc = new XmlDocument();
+            if( reader.Read() )
+            {
+                try
+                {
+                    doc.LoadXml( @"<root>" + reader[0] + @"</root>" );
+                }
+                catch( XmlException ) { } //do nothing it doesn't matter if the XML is invalid. The doc will just be an empty doc which works fine.
+            }
+            Response = doc.GetElementsByTagName( "row" );
+        }
+        public XmlNodeList Response { get; private set; }
         private bool Authed = false;
     }
 }
