d4b2f0d26f090543323fc83cbb45bb3a0503f143
3xFail <ryan.williams@oit.edu>
2016-03-18 11:43:21 -0700

    Finded a problem with the inventory calling the employee search page

--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPage.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPage.xaml.cs
@@ -39,7 +39,7 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
 
         private void SearchBtn_Click(object sender, RoutedEventArgs e)
         {
-            InvFrame.Navigate( new SearchEmployeePage() );
+            InvFrame.Navigate( new SearchInventoryPage() );
         }
 
         private void CatalogBtn_Click(object sender, RoutedEventArgs e)
--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPages/SearchInventoryPage.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPages/SearchInventoryPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CSharpClient;
+using SnapRegisters;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
 {
@@ -22,6 +26,32 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
     {
         public SearchInventoryPage()
         {
+            
+
+            m_table = new DataTable( "SearchCatalog" );
+            m_table.Columns.Add( "ProductID", typeof( int ) );
+            m_table.Columns.Add( "Name", typeof( string ) );
+            m_table.Columns.Add( "Price", typeof( double ) );
+            m_table.Columns.Add( "Barcode", typeof( string ) );
+            //Table.Columns.Add( "Stock", typeof(Int32) );
+            m_table.Columns.Add( "Active", typeof( bool ) );
+
+            //populates the response with the list of item nodes
+            DBInterface.GetAllProducts();
+
+            //loop though the list adding each row to the list
+            foreach( XmlNode node in DBInterface.Response )
+            {
+                var row = m_table.Rows.Add( new Object[] {
+                                            int.Parse( node.Get( "ProductID" ) )
+                                            , node.Get( "Name" )
+                                            , double.Parse( node.Get( "Price" ) )
+                                            , node.Get( "Barcode" )
+                                            //, int.Parse( node.Get( "Stock" ) ) need to add eventually: R
+                                            , node.Get( "Active" ) != "1"
+                } );
+            }
+            SearchCatalog.ItemsSource = m_table.DefaultView;
             InitializeComponent();
         }
 
@@ -34,7 +64,9 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
 
         private void Search_TextChanged( object sender, TextChangedEventArgs e )
         {
-
+            m_table.DefaultView.RowFilter = string.Format( "Name LIKE '%{0}%'", SearchBox.Text );
         }
+
+        private DataTable m_table;
     }
 }
