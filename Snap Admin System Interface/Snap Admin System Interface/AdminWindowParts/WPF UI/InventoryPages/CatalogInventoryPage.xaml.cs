6fbc169cedcc701186e0af3e8b0427c9e97b3f97
Jacob Asmuth <david.asmuth@oit.edu>
2016-03-19 04:02:50 -0700

    Added ability to filter Catalog on some criteria

--- Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPages/CatalogInventoryPage.xaml.cs
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/InventoryPages/CatalogInventoryPage.xaml.cs
@@ -26,9 +26,9 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
     /// Interaction logic for CatalogInventoryPage.xaml
     /// </summary>
     
-    public class Item: INotifyPropertyChanged
+    public class Item: INotifyPropertyChanged, ICloneable
     {
-        private string productid;
+        private int productid;
         private string name;
         private decimal price;
         private string barcode;
@@ -41,7 +41,19 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
                 PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
         }
 
-        public string ProductID
+        public object Clone()
+        {
+            return new Item()
+            {
+                ProductID = this.ProductID
+                , Name = this.Name
+                , Price = this.Price
+                , Barcode = this.Barcode
+                , Active = this.Active
+            };
+        }
+
+        public int ProductID
         {
             get { return productid; }
             set { productid = value; OnPropertyChanged( "ProductID" ); }
@@ -67,6 +79,9 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
             set { active = value; OnPropertyChanged( "Active" ); }
         }
 
+        public bool Changed { get; set; } = false;
+        public bool WasChanged { get; set; } = false;
+
     }
 
     public partial class CatalogInventoryPage : Page
@@ -83,7 +98,7 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
             foreach( XmlNode node in DBInterface.Response )
             {
                 data.Add( new Item() {
-                    ProductID = node.Get( "ProductID" )
+                    ProductID = int.Parse( node.Get( "ProductID" ) )
                     , Name = node.Get( "Name" )
                     , Price = decimal.Parse( node.Get( "Price" ) )
                     , Barcode = node.Get( "Barcode" )
@@ -91,16 +106,51 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
                     , Active = node.Get( "Active_Use" )[0] == '1'
                 } );
             }
-            Catalog.ItemsSource = data;
+            LoadItems();
         }
+
+        public bool ItemFilter( object o )
+        {
+            Item item = o as Item;
+
+            if( item == null )
+                return false;
+
+            if( !item.Name.Contains( NameSearchBox.Text ) )
+                return false;
+            if( !item.Barcode.Contains( BarcodeSearchBox.Text ) )
+                return false;
+            if( ActiveComboBox.SelectedIndex == 1 && !item.Active ) //Index 1 = Show active only
+                return false;
+            if( ActiveComboBox.SelectedIndex == 2 && item.Active ) //Index 2 = Show inactive only
+                return false; 
+
+            return true;
+        }
+
+        private void LoadItems()
+        {
+            ICollectionView data_cv = CollectionViewSource.GetDefaultView( data );
+            if( data_cv != null )
+            {
+                Catalog.ItemsSource = data_cv;
+                data_cv.Filter = ItemFilter;
+            }
+        }
+
 
         private void Commit( object sender, RoutedEventArgs e )
         {
+            
             Item item = ( (FrameworkElement)sender ).DataContext as Item;
+            item.Changed = false;
+            item.WasChanged = true;
+
+            LoadItems();
 
             try
             {
-                DBInterface.ModifyItem( int.Parse( item.ProductID ), item.Name, item.Barcode, item.Price, item.Active );
+                DBInterface.ModifyItem( item.ProductID, item.Name, item.Barcode, item.Price, item.Active );
                 MessageBox.Show( "Changes saved to database." );
             }
             catch( InvalidOperationException ex )
@@ -109,5 +159,56 @@ namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI.InventoryPages
             }
         }
 
+        Item _EditItem;
+
+        private void Catalog_BeginningEdit( object sender, DataGridBeginningEditEventArgs e )
+        {
+            Item item = e.Row.Item as Item;
+
+            _EditItem = new Item();
+            _EditItem.Name = item.Name;
+            _EditItem.Barcode = item.Barcode;
+            _EditItem.Active = item.Active;
+            _EditItem.Price = item.Price;
+        }
+
+        private void Catalog_CellEditEnding( object sender, DataGridCellEditEndingEventArgs e )
+        {
+            Item item = e.Row.Item as Item;
+            if( item.Price != _EditItem.Price || item.Name != _EditItem.Name || item.Barcode != _EditItem.Barcode || item.Active != _EditItem.Active )
+            {
+                e.Row.Background = new SolidColorBrush( Color.FromArgb( 128, 255, 0, 0 ) );
+                item.Changed = true;
+            }
+        }
+
+        private void Catalog_LoadingRow( object sender, DataGridRowEventArgs e )
+        {
+            Item item = e.Row.Item as Item;
+            if( item.Changed )
+                e.Row.Background = new SolidColorBrush( Color.FromArgb( 128, 255, 0, 0 ) );
+            else if( item.WasChanged )
+                e.Row.Background = new SolidColorBrush( Color.FromArgb( 128, 0, 255, 0 ) );
+        }
+
+        private void SearchBox_TextChanged( object sender, TextChangedEventArgs e )
+        {
+            LoadItems();
+        }
+
+        private void ActiveCheckBox_Checked( object sender, RoutedEventArgs e )
+        {
+            LoadItems();
+        }
+
+        private void ActiveCheckBox_Unchecked( object sender, RoutedEventArgs e )
+        {
+            LoadItems();
+        }
+
+        private void comboBox_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+            LoadItems();
+        }
     }
 }
