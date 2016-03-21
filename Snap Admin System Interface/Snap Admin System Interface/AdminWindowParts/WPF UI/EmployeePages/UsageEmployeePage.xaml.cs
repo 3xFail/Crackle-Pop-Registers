486dddfba9c1cee237bc31f5efdd81475260ffdb
Jacob Asmuth <david.asmuth@oit.edu>
2016-03-21 00:39:49 -0700

    added untracked files?

---
+++ Snap Admin System Interface/Snap Admin System Interface/AdminWindowParts/WPF UI/EmployeePages/UsageEmployeePage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SnapRegisters;
+using System.Xml;
+using System.Collections.ObjectModel;
+using CSharpClient;
+using System.ComponentModel;
+
+namespace Snap_Admin_System_Interface.AdminWindowParts.WPF_UI
+{
+    public class UsageData
+    {
+        public string Username { get; set; } = string.Empty;
+        public DateTime StartTime { get; set; }
+        public string Duration { get; set; }
+        public TimeSpan Duration_TimeSpan { get; set; } //for efficient filtering, so we don't need to convert every time we compare
+        public DateTime EndTime { get; set; }
+        public int ItemsSold { get; set; }
+        public string ItemsMin { get; set; }
+        public string TotalValue { get; set; }
+    }
+    /// <summary>
+    /// Interaction logic for UsageEmployeePage.xaml
+    /// </summary>
+    public partial class UsageEmployeePage :Page
+    {
+        public ObservableCollection<UsageData> data = new ObservableCollection<UsageData>();
+        public UsageEmployeePage()
+        {
+            InitializeComponent();
+            StartDatePicker.SelectedDate = DateTime.Now.AddMonths( -1 );
+            EndDatePicker.SelectedDate = DateTime.Now.AddDays( 1 );
+            PopulateList();
+        }
+
+        private void PopulateList()
+        {
+            data.Clear();
+            DBInterface.GetUsageStatistics( StartDatePicker.SelectedDate, EndDatePicker.SelectedDate );
+
+            foreach( XmlNode node in DBInterface.Response )
+            {
+                DateTime start = DateTime.Parse( node.Get( "LoginTime" ) );
+                DateTime stop = DateTime.Parse( node.Get( "LogoutTime" ) );
+                TimeSpan duration = stop - start;
+                int items_sold = int.Parse( node.Get( "ItemsSold" ) );
+
+                data.Add( new UsageData()
+                {
+                    Username = node.Get( "Username" ) + " (" + node.Get( "FName" ) + ' ' + node.Get( "LName" ) + ')'
+                    , StartTime = start
+                    , Duration = duration.ToString( @"hh\:mm\:ss" )
+                    , Duration_TimeSpan = duration
+                    , EndTime = stop
+                    , ItemsSold = items_sold
+                    , ItemsMin = ( items_sold / duration.TotalMinutes ).ToString( "F2" )
+                    , TotalValue = decimal.Parse( node.Get( "TotalSales" ) ).ToString( "C2" )
+                } );
+            }
+            LoadItems();
+        }
+
+        private void LoadItems()
+        {
+            ICollectionView data_cv = CollectionViewSource.GetDefaultView( data );
+            if( data_cv != null )
+            {
+                UsageGrid.ItemsSource = data_cv;
+                data_cv.Filter = UsageFilter;
+            }
+        }
+
+        private bool UsageFilter( object o )
+        {
+            UsageData data = o as UsageData;
+
+            if( !data.Username.Contains( UsernameSearchTextBox.Text ) )
+                return false;
+            if( DurationTextBox.Text != string.Empty && data.Duration_TimeSpan.TotalMinutes < int.Parse( DurationTextBox.Text ) )
+                return false;
+            return true;
+        }
+
+        private void SearchButton_Click( object sender, RoutedEventArgs e )
+        {
+            PopulateList();
+        }
+    }
+}
